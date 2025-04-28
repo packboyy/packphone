a9f5dae7689a0b22f728f8aa69ce0593d20431f3
packboy <andyjoiner2005@gmail.com>
2025-04-28 15:51:45 -0400

    removes and adds songs from index

--- musicplayer/index.py
+++ musicplayer/index.py
@@ -17,37 +17,76 @@ class index():
     music_index = None
     
     @staticmethod
+    def add_song_to_index(music_index, songpath):
+        tags = TinyTag.get(songpath, image=False)
+        artist = tags.artist or "Unknown"
+        album = tags.album or "Unknown"
+        title = tags.title or os.path.basename(songpath)
+        
+        if artist not in music_index["artists"]:
+            music_index["artists"][artist] = {"albums": {}}
+        
+        if album not in music_index["artists"][artist]["albums"]:
+            music_index["artists"][artist]["albums"][album] = {"songs": []}
+
+        music_index["artists"][artist]["albums"][album]["songs"].append({
+            "title": title,
+            "path": songpath
+        })
+
+    @staticmethod
     def load_or_build_index():
-        if index.music_index:
-            return index.music_index
-            
         if os.path.exists(index.INDEX_FILE):
             with open(index.INDEX_FILE, 'r') as f:
                 index.music_index = json.load(f)
+            # check for missing songs and remove them ---------------------------------
+            for artist in list(index.music_index["artists"].keys()):
+                for album in list(index.music_index["artists"][artist]["albums"].keys()):
+                    songs_to_keep = []
+                    for song_data in index.music_index["artists"][artist]["albums"][album]["songs"]:
+                        #add songs that exist to a list ---------------------------------
+                        if os.path.exists(song_data["path"]):
+                            songs_to_keep.append(song_data)
+                    # update songs list with only existing files ---------------------------------
+                    index.music_index["artists"][artist]["albums"][album]["songs"] = songs_to_keep
+                    
+                    #remove empty albums ---------------------------------
+                    if len(songs_to_keep) == 0:
+                        del index.music_index["artists"][artist]["albums"][album]
+                
+                # remove artists with no albums ---------------------------------
+                if len(index.music_index["artists"][artist]["albums"]) == 0:
+                    del index.music_index["artists"][artist]
+            
+            # scan for new songs ---------------------------------
+            for root, dirs, files in os.walk("music"):
+                for file in files:
+                    if file.endswith(".mp3"):
+                        songpath = os.path.join(root, file)
+                        # check if song already exists in index ---------------------------------
+                        song_exists = False
+                        for artist in index.music_index["artists"]:
+                            for album in index.music_index["artists"][artist]["albums"]:
+                                for song_data in index.music_index["artists"][artist]["albums"][album]["songs"]:
+                                    if song_data["path"] == songpath:
+                                        song_exists = True
+                                        break
+                        
+                        # add new song if not found ---------------------------------
+                        if not song_exists:
+                            index.add_song_to_index(index.music_index, songpath)
         else:
+            # create new index if it doesn't exist ---------------------------------
             index.music_index = {"artists": {}}
             for root, dirs, files in os.walk("music"):
                 for file in files:
                     if file.endswith(".mp3"):
                         songpath = os.path.join(root, file)
-                        tags = TinyTag.get(songpath, image=False)
-                        artist = tags.artist or "Unknown"
-                        album = tags.album or "Unknown"
-                        title = tags.title or os.path.basename(file)
-                        
-                        if artist not in index.music_index["artists"]:
-                            index.music_index["artists"][artist] = {"albums": {}}
-                        
-                        if album not in index.music_index["artists"][artist]["albums"]:
-                            index.music_index["artists"][artist]["albums"][album] = {"songs": []}
-                        
-                        index.music_index["artists"][artist]["albums"][album]["songs"].append({
-                            "title": title,
-                            "path": songpath
-                        })
-            
-            with open(index.INDEX_FILE, 'w') as f:
-                json.dump(index.music_index, f)
+                        index.add_song_to_index(index.music_index, songpath)
+        
+        # save updated index ---------------------------------
+        with open(index.INDEX_FILE, 'w') as f:
+            json.dump(index.music_index, f)
                 
         return index.music_index
 
